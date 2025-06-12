6ba0b816126eaa00941c4438f2e662c2dcd0a55b
Nina Howley <ninahowley04@gmail.com>
2025-06-12 12:21:02 -0400

    trying pages

---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+        page_title="Cookies Project",
+        initial_sidebar_state="expanded",
+        layout="wide")
+
+# Pages configuration setup - will show as part of the sidebar
+pages = {
+    "Menu": [
+        st.Page("introduction.py", title="Introduction"), #intro to cookies --> example db
+        st.Page("followalong.py", title="Follow Along"),
+    ]
+}
+
+pg = st.navigation(pages)
+pg.run()
