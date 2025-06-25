8a018a1523e42a40356cde5d9755bbb2689426bb
Nina Howley <ninahowley04@gmail.com>
2025-06-25 13:43:27 -0400

    fixed follow along removal

--- followalong.py
+++ followalong.py
@@ -64,8 +64,8 @@ if visualization == "Cookie Security":
     #creating cookie security pie charts
     #col1, col2 = st.columns((1,1))
     #with col1:
-    
-    
+
+
     #with col2:
     st.subheader("Secure vs. insecure cookies")
     st.write("Let's learn about cookie security! Below is a pie chart showing the proportions of your secure and insecure cookies.")
@@ -73,9 +73,33 @@ if visualization == "Cookie Security":
     st.write("\n\n**Secure cookies** are designed to **only be transmitted over HTTPS**, which means they are encrypted only when sent from the domain to server and are less vulnerable to interception. " \
     "Web browsers (or user agents) will only include the cookie in an **HTTPS request**, only if it is transmitted over a secure channel (likely HTTPS). HTTPS is secure because it uses encryption to " \
     "protect data in transit between the user's browser to server." \
-    "\n\nHowever, insecure cookies can be sent over HTTP, which transmits data in plain text, potentially exposing this information to attackers. " \
-    "\n\n In the database, to see if cookies are secure or insecure, look at the *is_secure* attribute. *is_secure* = 1 means it's secure and *is_secure* = 0 means it's insecure. ")
-    st.subheader("")
+    "\n\nHowever, **insecure cookies** can be sent over **HTTP**, which transmits data in **plain text**, potentially exposing this information to attackers. ")
+    vm.pie_chart(cookies)
+    st.write("In the database, to see if cookies are secure or insecure, look at the *is_secure* attribute. ***is_secure* = 1** means it's secure and ***is_secure* = 0** means it's not secure. ")
+
+    vm.double_bar(cookies)
+
+
+    st.subheader("SameSite Cookies")
+    st.write("The SameSite attribute is set in place to protect against Cross-Site Request Forgery (CSRF) attacks, where a malicious website tricks the browser into performing unwanted actions." \
+     "For example, hackers can inherit the user's cookies and send unauthorized commands to another website, appearing as a 'trusted user'." \
+     " SameSite is here to limit when cookies can be sent.")
+
+    st.write("*You can hover above the pie chart to see the exact count for each attribute.*")
+
+    c1, c2 = st.columns((1,1), gap = "large")
+    with c1:
+        vm.sameSite(cookies)
+        st.caption("*: Chrome treats unspecified sameSite attributes as lax.")
+    with c2:
+          option = st.selectbox("Now, let's dive a little deeper into what each of these values means!", ("None", "Lax", "Strict"))
+          if option == "None":
+               st.write("Cookies will be shared between sites with all cross-site requests, but this requires that the cookie is a Secure one. This attribute can be used for Third Party cookies.")
+          elif option == "Lax":
+               st.write("Cookies will be shared across domains, meaning that they will be sent for top-level navigations. For example, when you click on a link leading to the site, the cookie will be sent.")
+               st.write("Chrome's default is to treat the SameSite attribute as = Lax if it is missing.")
+          else:
+               st.write("Cookies will only be sent if the request originates from the same site. Examples of websites that use SameSite = Strict are financial service websites, where privacy of personal information is incredibly crucial.")
 
 if visualization == 'Persistent Cookies':
     st.header("Persistent Cookies")
@@ -85,8 +109,7 @@ if visualization == 'Persistent Cookies':
     with col1:
         vm.persistent_cookies(cookies)
     with col2:
-         st.write("Add information")
-    "\n\nHowever, **insecure cookies** can be sent over **HTTP**, which transmits data in **plain text**, potentially exposing this information to attackers. ")
+        st.write("Add information")
     vm.pie_chart(cookies)
     st.write("In the database, to see if cookies are secure or insecure, look at the *is_secure* attribute. ***is_secure* = 1** means it's secure and ***is_secure* = 0** means it's not secure. ")
 
@@ -146,11 +169,6 @@ if visualization == "Domain Exploration":
 
 # m.categorize_cookies(cookies)
 
-<<<<<<< HEAD
-vm.last_accessed(cookies)
-
-m.cookie_type(cookies)
-=======
 if visualization == "Persistent Cookies":
     vm.persistent_cookies(cookies)
 
@@ -168,4 +186,3 @@ with cookie_count:
        st.write("Paste https://www.wellesley.edu/ below to find out!")
        website = cookie_count.text_input('Enter a website:') 
        cookies_count = m.get_cookies(website)
->>>>>>> 68dbdfb1a7a10c2015c44cfb0842a000493bce1c
