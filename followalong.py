3f5aa5778d44aa92bcf3ae90ca6377005f6c8230
Nina Howley <ninahowley04@gmail.com>
2025-06-29 15:21:00 -0400

    cookie upload warning

--- followalong.py
+++ followalong.py
@@ -53,16 +53,18 @@ if isinstance(cookies, pd.DataFrame):
     with col1:
         m.display_raw_cookies(cookies)
 
-columns = ['creation_utc', 'host_key', 'top_frame_site_key', 'name', 
-        'value', 'encrypted_value', 'path', 'expires_utc',
-        'is_secure', 'is_httponly', 'last_access_utc', 'has_expires',
-        'is_persistent', 'priority', 'samesite', 'source_scheme',
-        'source_port', 'last_update_utc', 'source_type', 'has_cross_site_ancestor']
+    columns = ['creation_utc', 'host_key', 'top_frame_site_key', 'name', 
+            'value', 'encrypted_value', 'path', 'expires_utc',
+            'is_secure', 'is_httponly', 'last_access_utc', 'has_expires',
+            'is_persistent', 'priority', 'samesite', 'source_scheme',
+            'source_port', 'last_update_utc', 'source_type', 'has_cross_site_ancestor']
 
-selection = col2.selectbox(options=columns,label="Choose a column to learn more about.")
+    selection = col2.selectbox(options=columns,label="**Choose a column to learn more about.**")
 
-if selection:
-    col2.write(m.display_description(selection))
+    if selection:
+        col2.write(m.display_description(selection))
+else:
+    st.warning("Please upload your cookies before viewing your data.")
 
 st.divider()
 st.header("Part 3: Visualize your data")
